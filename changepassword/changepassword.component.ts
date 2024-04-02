162df454809f83a01b68db31fda9de969ff1e342
OlimaBernard <159028479+OlimaBernard@users.noreply.github.com>
2024-04-02 14:31:04 +0300

    Change Password Component

---
+++ changepassword/changepassword.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-changepassword',
+  templateUrl: './changepassword.component.html',
+  styleUrls: ['./changepassword.component.sass']
+})
+export class ChangepasswordComponent implements OnInit {
+  public showCurrentPassword = false;
+  public showNewPassword = false;
+  public showConfirmPassword = false;
+  public changePasswordForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.changePasswordForm = this.formBuilder.group({
+      currentPassword: ['', Validators.required],
+      newPassword: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    }, {
+      validator: this.passwordMatchValidator
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  togglePassword(field: string) {
+    switch (field) {
+      case 'current':
+        this.showCurrentPassword = !this.showCurrentPassword;
+        break;
+      case 'new':
+        this.showNewPassword = !this.showNewPassword;
+        break;
+      case 'confirm':
+        this.showConfirmPassword = !this.showConfirmPassword;
+        break;
+      default:
+        break;
+    }
+  }
+
+  passwordMatchValidator(formGroup: FormGroup) {
+    const newPassword = formGroup.get('newPassword')?.value;
+    const confirmPassword = formGroup.get('confirmPassword')?.value;
+
+    if (newPassword !== confirmPassword) {
+      formGroup.get('confirmPassword')?.setErrors({ passwordMismatch: true });
+    } else {
+      formGroup.get('confirmPassword')?.setErrors(null);
+    }
+  }
+
+  onSubmit() {
+    // Submit logic here
+  }
+}
