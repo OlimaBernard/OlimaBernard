23bbae887587f625683cb8d6af26ae948a56bbb4
OlimaBernard <159028479+OlimaBernard@users.noreply.github.com>
2024-04-16 09:32:13 +0300

    password strength meter added

--- changepassword/changepassword.component.ts
+++ changepassword/changepassword.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-changepassword',
@@ -7,15 +7,18 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./changepassword.component.sass']
 })
 export class ChangepasswordComponent implements OnInit {
-  public showCurrentPassword = false;
   public showNewPassword = false;
   public showConfirmPassword = false;
+  public showPasswordRequirementsPopup = false; // Added variable
   public changePasswordForm: FormGroup;
+  public passwordStrength: string = '';
+  public passwordStrengthText: string = '';
+  public passwordStrengthColor: string = '';
 
   constructor(private formBuilder: FormBuilder) {
     this.changePasswordForm = this.formBuilder.group({
-      currentPassword: ['', Validators.required],
-      newPassword: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      newPassword: ['', [Validators.required, this.passwordStrengthValidator()]],
       confirmPassword: ['', Validators.required]
     }, {
       validator: this.passwordMatchValidator
@@ -28,10 +31,11 @@ export class ChangepasswordComponent implements OnInit {
   togglePassword(field: string) {
     switch (field) {
       case 'current':
-        this.showCurrentPassword = !this.showCurrentPassword;
-        break;
       case 'new':
         this.showNewPassword = !this.showNewPassword;
+        if (this.showNewPassword) {
+          this.showPasswordRequirementsPopup = true; // Show requirements popup when new password field is clicked
+        }
         break;
       case 'confirm':
         this.showConfirmPassword = !this.showConfirmPassword;
@@ -54,5 +58,103 @@ export class ChangepasswordComponent implements OnInit {
 
   onSubmit() {
     // Submit logic here
+  }
+
+  passwordStrengthValidator() {
+    const spaceRegex = /\s/; // Regex for spaces
+
+    const spaceValidator = (control: FormControl): { [key: string]: boolean } | null => {
+      const value: string = control.value;
+      if (value.trim().length === 0) {
+        return { 'spaces': true };
+      }
+      return null;
+    };
+
+    return (control) => {
+      const password = control.value;
+      const minLength = 8; // Minimum password length
+      const uppercaseRegex = /[A-Z]/; // Regex for uppercase letters
+      const lowercaseRegex = /[a-z]/; // Regex for lowercase letters
+      const numberRegex = /[0-9]/; // Regex for numbers
+      const specialCharRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/; // Regex for special characters
+
+      const isStrong = password.length >= minLength &&
+        uppercaseRegex.test(password) &&
+        lowercaseRegex.test(password) &&
+        numberRegex.test(password) &&
+        !spaceRegex.test(password) &&
+        specialCharRegex.test(password);
+
+      if (!isStrong) {
+        return { 'weakPassword': true };
+      }
+      return null;
+    };
+  }
+
+  updatePasswordStrength(): void {
+    const newPassword = this.changePasswordForm.get('newPassword')?.value;
+    this.passwordStrength = this.calculatePasswordStrength(newPassword);
+    this.updatePasswordStrengthText();
+  }
+
+  calculatePasswordStrength(password: string): string {
+    const minLength = 8;
+    const minUppercase = 1;
+    const minLowercase = 1;
+    const minNumbers = 1;
+    const minSpecialChars = 1;
+
+    let score = 0;
+
+    if (password.length >= minLength) {
+      score += 1;
+    }
+
+    if (/[A-Z]/.test(password)) {
+      score += 1;
+    }
+
+    if (/[a-z]/.test(password)) {
+      score += 1;
+    }
+
+    if (/\d/.test(password)) {
+      score += 1;
+    }
+
+    if (/[^A-Za-z0-9]/.test(password)) {
+      score += 1;
+    }
+
+    if (score <= 2) {
+      return 'weak';
+    } else if (score <= 4) {
+      return 'medium';
+    } else {
+      return 'strong';
+    }
+  }
+
+  updatePasswordStrengthText(): void {
+    switch (this.passwordStrength) {
+      case 'weak':
+        this.passwordStrengthText = 'Weak Password';
+        this.passwordStrengthColor = 'red';
+        break;
+      case 'medium':
+        this.passwordStrengthText = 'Good Password';
+        this.passwordStrengthColor = 'orange';
+        break;
+      case 'strong':
+        this.passwordStrengthText = 'Strong Password';
+        this.passwordStrengthColor = 'green';
+        break;
+      default:
+        this.passwordStrengthText = '';
+        this.passwordStrengthColor = '';
+        break;
+    }
   }
 }
